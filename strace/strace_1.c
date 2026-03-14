d643ac1c1ea3270aefee4d8be0749a6d56a06538
Akhigbesimeon <s.akhigbe@alustudent.com>
2026-03-14 12:19:57 +0200

    strace_1.c file added

--- strace/strace_1.c
+++ strace/strace_1.c
@@ -1,55 +1,69 @@
 #include <stdio.h>
-#include <sys/ptrace.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/user.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "syscalls.h"
-/*#include <sys/syscall.h>*/
-
 
 /**
- * main - traces a process and prints system call numbers as they're called
- * @argc: argument count
- * @argv: argument array
- * @envp: environment parameters
- * Return: 0 on success | 1 on failure (not enough arguments)
- * author: Frank Onyema Orji
- **/
+ * main - Traces a command and prints syscall names
+ * @argc: Argument count
+ * @argv: Argument vector
+ * @envp: Environment variables
+ *
+ * Return: 0 on success, 1 on failure
+ */
 int main(int argc, char *argv[], char *envp[])
 {
-	int i, status;
-	struct user_regs_struct regs;
-	pid_t pid;
+    pid_t child;
+    int status, is_entry = 1;
+    struct user_regs_struct regs;
 
-	if (argc < 2)
-	{
-		fprintf(stderr, "Usage: %s <full_path> [path_args]\n", argv[0]);
-		return (1);
-	}
-	setbuf(stdout, NULL);
-	pid = fork();
+    if (argc < 2)
+        return (1);
 
-	if (pid == 0)
-	{
-		printf("execve");
-		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
-		execve(argv[1], argv + 1, envp);
-	}
-	else
-	{
-		for (status = 1, i = 1; !WIFEXITED(status); i ^= 1)
-		{
-			ptrace(PT_SYSCALL, pid, NULL, NULL);
-			wait(&status);
-			ptrace(PT_GETREGS, pid, NULL, &regs);
-			if (i)
-				printf("\n");
-			else
-				printf("%s", syscalls_64_g[regs.orig_rax].name);
-		}
-	}
+    child = fork();
+    if (child == -1)
+        return (1);
 
-	return (0);
+    if (child == 0)
+    {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        execve(argv[1], argv + 1, envp);
+        perror("execve");
+        exit(1);
+    }
+    else
+    {
+        waitpid(child, &status, 0);
+        ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
+
+        while (1)
+        {
+            if (ptrace(PTRACE_SYSCALL, child, NULL, NULL) == -1)
+                break;
+            waitpid(child, &status, 0);
+
+            if (WIFEXITED(status) || WIFSIGNALED(status))
+                break;
+
+            if (is_entry)
+            {
+                if (ptrace(PTRACE_GETREGS, child, NULL, &regs) == -1)
+                    break;
+                
+                /* Print the name from our lookup table */
+                printf("%s\n", syscalls_64[regs.orig_rax].name);
+                fflush(stdout);
+                is_entry = 0;
+            }
+            else
+            {
+                is_entry = 1;
+            }
+        }
+    }
+    return (0);
 }
